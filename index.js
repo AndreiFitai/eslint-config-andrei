be62209af2fbaf40b250d60f5bf602ec48312d31
Andrei Fitai <fitai.andrei@gmail.com>
2021-11-09 13:43:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["import", "prettier", "@typescript-eslint", "immutable"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    "no-console": 1,
+    curly: [2, "multi-line", "consistent"],
+    "import/order": 1,
+    "import/no-duplicates": 1,
+    "import/newline-after-import": 1,
+    "import/no-useless-path-segments": 1,
+    "import/no-extraneous-dependencies": 1,
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      1,
+      { args: "after-used", varsIgnorePattern: "^_" },
+    ],
+    "immutable/no-let": 2,
+    "immutable/no-this": 2,
+    "immutable/no-mutation": 2,
+  },
+  ignorePatterns: [".eslintrc.js"],
+};
